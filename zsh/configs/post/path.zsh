caeec482a6f154166e8e6fac10e1f2ba022bde7f
Hallelujah <hery@rails-royce.org>
2026-03-22 21:57:44 +0100

    Activate shell env integration for mise and homebrew

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,7 +1,24 @@
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.local/bin:$HOME/.bin:/usr/local/sbin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
 export -U PATH
+
+# Activate Mise for system that has it (MacOS, Fedora)
+if command -v mise >/dev/null; then
+  mise activate zsh
+fi
+
+arch="$(uname -m)"
+if [ "$arch" = "arm64" ]; then
+  HOMEBREW_PREFIX="/opt/homebrew"
+else
+  HOMEBREW_PREFIX="/usr/local"
+fi
+
+# Activate Homebrew for system that has it (MacOS)
+if [ -f "$HOMEBREW_PREFIX/bin/brew" ]; then
+  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+fi

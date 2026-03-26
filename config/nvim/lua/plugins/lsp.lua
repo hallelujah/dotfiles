b1ed7729d2de0e508f860e2661dad0f12e3f7bf8
Hallelujah <hery@rails-royce.org>
2026-03-27 00:03:26 +0100

    Do not install ruby-lsp and rubocop with Mason
    See https://github.com/mason-org/mason.nvim/issues/1777#issuecomment-3092418054

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,8 +3,23 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        ["*"] = {
+          capabilities = {
+            general = {
+              positionEncodings = { "utf-8", "utf-16" },
+            },
+          },
+        },
         taplo = {
           root_dir = require("lspconfig.util").root_pattern("*.toml", ".git", "Cargo.toml"),
+        },
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.local/share/mise/shims/ruby-lsp") },
+        },
+        rubocop = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.local/share/mise/shims/rubocop"), "--lsp" },
         },
       },
     },

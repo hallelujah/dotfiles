38ee86e2860c462a178d711ed6dc05db2f70f9ac
Hallelujah <hery@rails-royce.org>
2026-03-30 16:44:20 +0200

    Use ai-copilot-native
    Update to Neovim 0.12+ required

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,39 @@
+local function cmd_path(cmd)
+  local is_nixos = vim.fn.filereadable("/etc/NIXOS") == 1
+    or (
+      vim.fn.filereadable("/etc/os-release") == 1
+      and vim.fn.match(vim.fn.readfile("/etc/os-release"), "ID=nixos") >= 0
+    )
+  if is_nixos then
+    return cmd
+  else
+    return vim.fn.expand("~/.local/share/mise/shims/" .. cmd)
+  end
+end
+
+local capabilities = {
+  offsetEncoding = { "utf-16" },
+  positionEncodings = { "utf-16" },
+}
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         ["*"] = {
-          capabilities = {
-            general = {
-              positionEncodings = { "utf-8", "utf-16" },
-            },
-          },
+          capabilities = capabilities,
         },
         taplo = {
           root_dir = require("lspconfig.util").root_pattern("*.toml", ".git", "Cargo.toml"),
         },
         ruby_lsp = {
           mason = false,
-          cmd = { vim.fn.expand("~/.local/share/mise/shims/ruby-lsp") },
+          cmd = { cmd_path("ruby-lsp") },
         },
         rubocop = {
           mason = false,
-          cmd = { vim.fn.expand("~/.local/share/mise/shims/rubocop"), "--lsp" },
+          cmd = { cmd_path("rubocop"), "--lsp" },
         },
       },
     },

97e8b1f71e770dc15a76cf5d60ab39d616423433
Hallelujah <hery@rails-royce.org>
2026-04-01 13:31:08 +0200

    Allow use of bundled rubocop and ruby-lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,16 @@
 local function cmd_path(cmd)
+  if vim.fn.filereadable("Gemfile") == 1 then
+    return { "bundle", "exec", cmd }
+  end
   local is_nixos = vim.fn.filereadable("/etc/NIXOS") == 1
     or (
       vim.fn.filereadable("/etc/os-release") == 1
       and vim.fn.match(vim.fn.readfile("/etc/os-release"), "ID=nixos") >= 0
     )
   if is_nixos then
-    return cmd
+    return { cmd }
   else
-    return vim.fn.expand("~/.local/share/mise/shims/" .. cmd)
+    return { vim.fn.expand("~/.local/share/mise/shims/" .. cmd) }
   end
 end
 
@@ -29,11 +32,11 @@ return {
         },
         ruby_lsp = {
           mason = false,
-          cmd = { cmd_path("ruby-lsp") },
+          cmd = cmd_path("ruby-lsp"),
         },
         rubocop = {
           mason = false,
-          cmd = { cmd_path("rubocop"), "--lsp" },
+          cmd = vim.list_extend(cmd_path("rubocop"), { "--lsp" }),
         },
       },
     },

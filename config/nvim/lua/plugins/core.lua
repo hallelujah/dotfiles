07149812d1412e61a58e4c8b5fe49299974edc6a
Hallelujah <hery@rails-royce.org>
2026-04-03 01:06:11 +0200

    Run ruby_lsp lazily

--- config/nvim/lua/plugins/core.lua
+++
@@ -1,9 +0,0 @@
--- Use Snazzy as our default color scheme
-return {
-  {
-    "LazyVim/LazyVim",
-  },
-
-  -- start ruby lsp server right away
-  { "neovim/nvim-lspconfig", opts = { servers = { ruby_lsp = {} } } },
-}
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -19,9 +19,40 @@ local capabilities = {
   positionEncodings = { "utf-16" },
 }
 
+-- Helper to check if the command is available
+local function has_gem_or_cmd(cmd)
+  -- 1. Check if it's in the Gemfile.lock (for "bundle exec" projects)
+  if vim.fn.filereadable("Gemfile.lock") == 1 then
+    local lockfile_content = table.concat(vim.fn.readfile("Gemfile.lock"), "\n")
+    -- Look for the gem name in the specs section
+    if lockfile_content:find("%s" .. cmd .. "%s") then
+      return true
+    end
+  end
+
+  -- 2. Fallback: Check if it's executable via mise or system path
+  local path = cmd_path(cmd)
+  return vim.fn.executable(path[1]) == 1 or vim.fn.executable(cmd) == 1
+end
+
+-- Logic to determine which server to use
+local ruby_server = "ruby_lsp"
+local ruby_server_cmd = cmd_path("ruby-lsp")
+
+if has_gem_or_cmd("solargraph") then
+  ruby_server = "solargraph"
+  ruby_server_cmd = cmd_path("solargraph")
+end
+vim.notify("ruby lsp server: " .. ruby_server, vim.log.levels.INFO)
+
 return {
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      -- This tells LazyVim's Ruby extra which server to use globally
+      vim.g.lazyvim_ruby_lsp = ruby_server
+      vim.g.lazyvim_ruby_formatter = "rubocop"
+    end,
     opts = {
       servers = {
         ["*"] = {
@@ -30,10 +61,12 @@ return {
         taplo = {
           root_dir = require("lspconfig.util").root_pattern("*.toml", ".git", "Cargo.toml"),
         },
-        ruby_lsp = {
+        [ruby_server] = {
           mason = false,
-          cmd = cmd_path("ruby-lsp"),
+          cmd = ruby_server_cmd,
         },
+        -- [ruby_server == "solargraph" and "ruby_lsp" or "solargraph"] = { enabled = false },
+
         rubocop = {
           mason = false,
           cmd = vim.list_extend(cmd_path("rubocop"), { "--lsp" }),

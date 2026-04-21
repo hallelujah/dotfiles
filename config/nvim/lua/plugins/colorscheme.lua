bc9428d5f1d1cc3f40111c71fa372b9fdd719c1e
Hallelujah <hery@rails-royce.org>
2026-04-21 09:16:17 +0200

    Use kanagawa as colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,19 +3,42 @@ return {
   { "alexwu/nvim-snazzy" },
   {
     "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
+    priority = 996,
     config = function()
       require("onedark").setup({
         style = "deep",
         toggle_style_key = "<leader>ts", -- toggle style
       })
-      require("onedark").load()
+      -- require("onedark").load()
     end,
   },
   {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+  },
+  {
     "embark-theme/vim",
-    lazy = false,
-    priority = 1000,
+    priority = 997,
     name = "embark",
+  },
+  {
+    "rmehri01/onenord.nvim",
+    priority = 998,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 999,
+  },
+  {
+    "sainnhe/everforest",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
   },
 }

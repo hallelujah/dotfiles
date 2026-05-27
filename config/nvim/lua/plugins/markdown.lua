cc68516b59d0793de66a6cefc45872d2e75f75b7
Hallelujah <hery@rails-royce.org>
2026-05-27 13:15:12 +0200

    Add markdown-preview.nvim with explorer picker action

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,7 @@
+local function is_markdown_path(path)
+  return path and path:match("%.[Mm][Dd]$") ~= nil
+end
+
 return {
   {
     "hedyhli/markdown-toc.nvim",
@@ -11,5 +15,49 @@ return {
         pattern = "*.{md,mdown,mkd,mkdn,markdown,mdwn}",
       },
     },
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = {
+      {
+        "<leader>md",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
+        win = {
+          list = {
+            keys = {
+              ["<leader>md"] = "markdown_preview",
+            },
+          },
+        },
+        actions = {
+          markdown_preview = function(picker, item)
+            if not item then
+              return
+            end
+            local path = item.file or item.path or item._path
+            if not is_markdown_path(path) then
+              vim.notify("Not a markdown file: " .. tostring(path), vim.log.levels.WARN)
+              return
+            end
+            picker:close()
+            vim.cmd("edit " .. vim.fn.fnameescape(path))
+            vim.cmd("MarkdownPreviewToggle")
+          end,
+        },
+      })
+    end,
   },
 }
